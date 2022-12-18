f66b7f6e09e042745866f31e3eb1db00995736b5
Mateusz <mateusz.grzybowski98@gmail.com>
2022-12-18 17:43:53 +0100

    Created navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="App">
+        <Box
+          width="400px"
+          sx={{
+            width: { xl: '1488px' },
+          }}
+          m="auto"
+        >
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/exercise" element={<ExerciseDetail />} />
+          </Routes>
+          <Footer />
+        </Box>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
